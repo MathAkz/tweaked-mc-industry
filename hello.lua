d938ce210972bd6b7a8911334b86bc5aea0269ff
MathAkz <115894221+MathAkz@users.noreply.github.com>
2026-08-23 19:52:54 +0100

    Update monitor text to new messages

--- hello.lua
+++ hello.lua
@@ -5,8 +5,8 @@ monitor.setTextScale(2)
 
 monitor.setCursorPos(1, 1)
 monitor.setTextColor(colors.cyan)
-monitor.write("STATECH")
+monitor.write("waifuus nordestinas")
 
 monitor.setCursorPos(1, 3)
-monitor.setTextColor(colors.lime)
-monitor.write("Factory Online!")
+monitor.setTextColor(colors.red)
+monitor.write("ntron")
